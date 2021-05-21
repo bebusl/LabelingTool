43a1ae784bb69de3743158da9de489d29e12c878
dungbik <q1w2e30630@gmail.com>
2021-05-21 14:41:35 +0900

    토크나이저 변경

--- filesaver.py
+++ filesaver.py
@@ -1,7 +1,22 @@
 import pickle
 import shutil
 import datetime
-from transformers import BertTokenizer
+from transformers import AutoTokenizer
+import re
+import emoji
+from soynlp.normalizer import repeat_normalize
+
+emojis = ''.join(emoji.UNICODE_EMOJI.keys())
+pattern = re.compile(f'[^ .,?!/@$%~％·∼()\x00-\x7Fㄱ-ㅣ가-힣{emojis}]+')
+url_pattern = re.compile(
+    r'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)')
+
+def clean(x):
+    x = pattern.sub(' ', x)
+    x = url_pattern.sub('', x)
+    x = x.strip()
+    x = repeat_normalize(x, num_repeats=2)
+    return x
 
 def save_backup():
     suffix=datetime.datetime.now().strftime("%m%d_%H%M%S")
@@ -15,12 +30,16 @@ def save_backup():
 def fileLoad(path="./sample.txt"):
     save_backup()
     reviews=[]
-    inputFile = open(path,'rt',encoding='UTF8')
+    inputFile = open(path,'r')
     lines = inputFile.readlines()
+
+    tokenizer = AutoTokenizer.from_pretrained("beomi/kcbert-large")
+
     for line in lines:
-        line = line.replace("\n","")
-        ptr_tokenizer = BertTokenizer.from_pretrained("pretrained/vocab_snu_char16424.txt", do_lower_case=False)
-        words = ptr_tokenizer.tokenize(line)
+        line = clean(line.replace("\n",""))
+        print(line)
+        words = tokenizer.tokenize(line)
+        print(words)
         words = [word.replace("#", "") for word in words]
         reviews.append(words)
         #여기서 words를 화면에 쫘라락 보여주고, 거기서 선택할 수 있게 한 담에 result.txt에 저장시켜야 함.
